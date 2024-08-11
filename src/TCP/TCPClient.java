14b36de6718311f83109ec1e8b91ab86d3098b52
markogeorgiev <markogeorgiev9@gmail.com>
2024-08-11 23:08:03 +0200

    Add all files

---
+++ src/TCP/TCPClient.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Random;
+import java.util.Scanner;
+
+public class TCPClient extends Thread{
+    Socket socket;
+    BufferedReader reader;
+    BufferedWriter writer;
+    Random random;
+
+    public TCPClient(Socket socket) throws IOException {
+        this.socket = socket;
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+
+    @Override
+    public void run() {
+        String msgFromServer;
+        try {
+            while ((msgFromServer = reader.readLine()) != null){
+                System.out.println(msgFromServer);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+                reader.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendData(){
+        String msgFromServer;
+        try {
+            Scanner sc = new Scanner(System.in);
+            String msgToSend;
+
+            while ((msgToSend = sc.nextLine()) != null){
+                writer.write(msgToSend);
+                writer.newLine();
+                writer.flush();
+
+                if (msgToSend.equals("END")){
+                    break;
+                }
+            }
+            //Receive 7 communication data
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                writer.close();
+                reader.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 9002);
+        TCPClient client = new TCPClient(socket);
+        client.start();
+        client.sendData();
+    }
+}
---
+++ src/TCP/TCPServer.java
@@ -0,0 +1,105 @@
+package TCP;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class TCPServer extends Thread{
+    private int port;
+    public static int NUM_MESSAGES;
+    public static int NUM_CLIENTS;
+
+    public TCPServer(int port){
+        this.port = port;
+    }
+
+    public static void printNuMessages(){
+
+    }
+
+    @Override
+
+    public void run() {
+        System.out.println("TCP Server is starting...");
+
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(this.port);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("TCP server has started!");
+        System.out.println("Waiting for clients to connect");
+
+        try {
+            while (true){
+                Socket socket = serverSocket.accept();
+                System.out.println("Accepted connection from " + socket.getRemoteSocketAddress());
+                TCPServer.NUM_CLIENTS++;
+                new WorkerThread(socket).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        TCPServer server = new TCPServer(9002);
+        server.start();
+    }
+}
+
+class WorkerThread extends Thread {
+    Socket socket;
+    BufferedReader reader;
+    BufferedWriter writer;
+
+    public WorkerThread(Socket socket) throws IOException {
+        this.socket = socket;
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+
+    @Override
+    public void run() {
+        String receivedLine = null;
+        try {
+            while (!(receivedLine = reader.readLine()).isEmpty()){
+                TCPServer.NUM_MESSAGES++;
+                System.out.printf("%d. From %s: %s\n",TCPServer.NUM_MESSAGES, socket.getInetAddress(), receivedLine);
+                writer.write(String.format("Message '%s' received\n", receivedLine));
+                writer.flush();
+            }
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (socket != null){
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (reader != null){
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (writer != null){
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+}
